4702a9e9507fbb3d3e0a0f7b7e7f2d7ce813a05c
rawhide-kobayashi <77038961+rawhide-kobayashi@users.noreply.github.com>
2021-03-20 13:22:43 -0500

    Add files via upload

---
+++ arch_install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+HOSTNAME=$1
+
+timedatectl set-ntp true
+
+(
+echo g
+echo n
+echo  
+echo  
+echo +1M
+echo n
+echo  
+echo  
+echo  
+echo t
+echo 1
+echo 4
+echo w
+) | fdisk /dev/vda
+
+mkfs.xfs -f -b size=4k -s size=4k /dev/vda2
+
+mount /dev/vda2 /mnt
+
+pacstrap /mnt base linux-lts xfsprogs nano dhcpcd netctl sudo openssh grub
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cat <<EOF > /mnt/install_part_2.sh
+
+#!/bin/bash
+
+HOSTNAME=$1
+
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+
+sed -i '/#en_US.UTF-8 UTF-8/c\en_US.UTF-8 UTF-8' /etc/locale.gen
+sed -i '/#ja_JP.UTF-8 UTF-8/c\ja_JP.UTF-8 UTF-8' /etc/locale.gen
+locale-gen
+
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+echo $HOSTNAME > /etc/hostname
+echo -e "127.0.0.1 localhost\n::1       localhost\n127.0.1.1 ${HOSTNAME}.gensoukyou.neet.works ${HOSTNAME}" > /etc/hosts
+
+echo -e "Description='enp1s0 dhcp'\nInterface=enp1s0\nConnection=ethernet\nIP=dhcp\nDHCPClient=dhcpcd" > /etc/netctl/enp1s0
+netctl enable enp1s0
+systemctl enable sshd
+
+(
+echo root
+echo root
+) | passwd root
+
+mkinitcpio -P
+grub-install /dev/vda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+EOF
+
+chmod +x /mnt/install_part_2.sh
+arch-chroot /mnt /install_part_2.sh $HOSTNAME
+
+echo 'great success'
---
+++ ipa_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir /etc/krb5.conf.d
+ln -sf /usr/bin/true /usr/bin/authselect
+mkdir /etc/authselect
+cp /etc/nsswitch.conf /etc/authselect/user-nsswitch.conf
+mkdir /usr/share/ipa/client
+mkdir -p /usr/share/ipa/client
+echo -e '[libdefaults]\n    spake_preauth_groups = edwards25519' > /usr/share/ipa/client/freeipa.template
+
+sed -i '82s/.*/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+sed -i '4s/.*/passwd: sss files systemd/' /etc/nsswitch.conf 
+sed -i '5s/.*/group: sss files [SUCCESS=merge] systemd/' /etc/nsswitch.conf
+sed -i '6s/.*/shadow: files sss/' /etc/nsswitch.conf
+sed -i '14s/.*/services: sss files/' /etc/nsswitch.conf
+sed -i '18s/.*/netgroup: sss files/' /etc/nsswitch.conf
+sed -i '18 a automount: sss files' /etc/nsswitch.conf
+sed -i '19 a sudoers: files sss' /etc/nsswitch.conf
+
+sed -i '5 a auth       sufficient                  pam_sss.so           forward_pass' /etc/pam.d/system-auth
+sed -i '15 a account    [default=bad success=ok user_unknown=ignore authinfo_unavail=ignore] pam_sss.so' /etc/pam.d/system-auth
+sed -i '21 a password   sufficient                  pam_sss.so           use_authtok' /etc/pam.d/system-auth
+sed -i '26 a session    required                    pam_mkhomedir.so skel=/etc/skel/ umask=0077' /etc/pam.d/system-auth
+sed -i '29 a session    optional                    pam_sss.so' /etc/pam.d/system-auth
+
+sed -i '2 a auth            sufficient      pam_sss.so   forward_pass' /etc/pam.d/su
+sed -i '7 a account         [default=bad success=ok user_unknown=ignore authinfo_unavail=ignore] pam_sss.so' /etc/pam.d/su
+sed -i '9 a session         optional        pam_sss.so' /etc/pam.d/su
+
+sed -i '1 a auth            sufficient      pam_sss.so' /etc/pam.d/sudo
+sed -i '1 a password        sufficient      pam_sss.so' /etc/pam.d/passwd
+
+sed -i '12 a ldap_sudo_search_base = ou=wheel,dc=gensoukyou,dc=neet,dc=works' /etc/sssd/sssd.conf
